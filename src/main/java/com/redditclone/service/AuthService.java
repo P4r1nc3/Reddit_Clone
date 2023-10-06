dc5bdbb21c08ce4cb35ee15fe198b58b2a421b4a
parince <konrad.tupta@gmail.com>
2023-10-06 21:42:48 +0200

    Created generateVerificationToken inside AuthService

--- src/main/java/com/redditclone/service/AuthService.java
+++ src/main/java/com/redditclone/service/AuthService.java
@@ -1,14 +1,17 @@
 package com.redditclone.service;
 
 import com.redditclone.dto.RegisterRequest;
+import com.redditclone.model.NotificationEmail;
 import com.redditclone.model.User;
+import com.redditclone.model.VerificationToken;
 import com.redditclone.repository.UserRepository;
+import com.redditclone.repository.VerificationTokenRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +20,8 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
@@ -27,5 +32,21 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please Activate your Account",
+                user.getEmail(), "Thank you for signing up to Spring Reddit, " +
+                "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
